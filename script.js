e0ce8511d3cb8ee5c6f6fdd0a403733b45b6d39f
zasa87 <zasa.arte@gmail.com>
2023-03-29 15:05:16 -0500

    toastify

--- script.js
+++ script.js
@@ -8,43 +8,55 @@ class Producto{
         this.stock = stock;
         this.imagen = imagen;
     }
+    restaStock() {
+        this.stock = this.stock - 1
+        console.log(`el stock de ${this.nombre} ha sido actualizado`)
+    }
 }
 
 //Instanciar (Crear) productos objetos
 
-const ilustracion = new Producto(000, 'Ilustración', 500, 10, 'ilustración.jpg');
-const impresion = new Producto(001, 'Impresión', 150, 1000, 'impresionVinilo.jpg');
-const oleo = new Producto(002, 'Óleo', 2000, 5, 'CuadroÓleo.jpg');
-const pinturaAcrilica = new Producto(3, 'PinturaAcrilica', 1000, 10, 'CuadroAcrilico.jpg');
-const acuarela = new Producto(004, 'Acuarela', 800, 10, 'Acuarela.jpg');
+const producto0 = new Producto(0, 'Ilustración', 500, 10, 'ilustración.jpg');
+const producto1 = new Producto(1, 'Impresión', 150, 1000, 'impresionVinilo.jpg');
+const producto2 = new Producto(2, 'Óleo', 2000, 5, 'CuadroÓleo.jpg');
+const producto3 = new Producto(3, 'PinturaAcrilica', 1000, 10, 'CuadroAcrilico.jpg');
+const producto4 = new Producto(4, 'Acuarela', 800, 10, 'Acuarela.jpg');
 
-const productos = [ilustracion, impresion, oleo, pinturaAcrilica, acuarela];
+const productos = [producto0, producto1, producto2, producto3, producto4];
 
 
 //Función para insertar Cards con información DOM
 //Card con Boostrap
 
 const cardBoostrap = (listaStock) => {
-    document.body.innerHTML = ``
-
-    for (e of listaStock){
-        let card = document.createElement("div")
-        card.innerHTML = `
-        <div class="card" style="width: 18rem;">
-        <img src="./img/${e.imagen}" class="card-img-top" alt="Acá va la imagen de ${e.nombre}">
-        <div class="card-body">
-        <h5 class="card-title">${e.nombre}</h5>
-        <p class="card-text">Llevatelo por tan solo ${e.precio}</p>
-        <input type="button" onclick="agregaCarrito(${e.id})" class="btn btn-primary" value="Compra ya, sólo quedan ${e.stock}">
-        </div>
-    </div>`
+    for (e of listaStock) {
+        let card = document.createElement("div");
+        card.innerHTML = `<div class="card text-center  mb-3" style="width: 18rem;">
+                            <img class="card-img-top" src="./img/${e.imagen}"  alt="Acá va la imagen de ${e.nombre}">
+                            <div class="card-body">
+                            <h5 class="card-title">${e.nombre}</h5>
+                            <p class="card-text">Llevalo por tan solo ${e.precio}</p>                                
+                            <input type="button" onclick="toastCarrito(${e.id})" class="btn btn-dark" data-bs-toggle="button" value="Agrega al carrito">
+                            </div>
+                        </div>`;
+        
         document.body.append(card)
+        
     }
 }
 
 cardBoostrap(productos)
 
- 
+function toastCarrito(prod) {
+Toastify({
+    text: `Agregado ${prod} al carrito`,
+    duration: 3000,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right" // `left`, `center` or `right`
+
+}).showToast();
+}
+
 const arrayCarrito = []
 
 class ObjCarrito{
@@ -79,7 +91,7 @@ function agregaCarrito(prod) {
 function verCarrito() {
     document.body.innerHTML = ``
     
-    for (item of verCarrito) { //producto=0, cant=1/2/3
+    for (item of arrayCarrito) { //producto=0, cant=1/2/3
         let card = document.createElement("div")
         let datosProd = productos.find(elem => elem.id == item.producto)
 
@@ -95,131 +107,12 @@ function verCarrito() {
     }
 }
 
+
+const arrayProductos = [producto0, producto1, producto2, producto3, producto4]
+
 const guardarLS = (clave, valor) => { localStorage.setItem(clave, valor) }
 
 for (const producto of arrayProductos) {
     guardarLS(producto.id, JSON.stringify(producto))
 }
     
-
-function carrito() {
-    const divisa = '$';
-    const DOMitems = document.querySelector('#items');
-    const DOMcarrito = document.querySelector('#carrito');
-    const DOMtotal = document.querySelector('#total');
-    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
-}
-
-const carrito = [divisa, DOMitems, DOMcarrito, DOMtotal,DOMbotonVaciar]
-
- const itemcarrito= (carrito) => {
-    document.body.innerHTML = ``
-    for (item of carrito){ //producto : 0, cantidad: 1/2/3
-        let card = document.createElement("div")
-        let datosProd = productos.find(elem => elem.id == item.producto)
-        card.innerHTML = `<div class="container">
-                <div class="row">
-                <!-- Elementos generados a partir del JSON -->
-                <main id="items" class="col-sm-8 row"></main>
-                <!-- Carrito -->
-                <aside class="col-sm-4">
-                    <h2>Carrito</h2>
-                    <!-- Elementos del carrito -->
-                    <ul id="carrito" class="list-group"></ul>
-                    <hr>
-                    <!-- Precio total -->
-                    <p class="text-right">Total: <span id="total"></span>&dollar;</p>
-                    <button id="boton-vaciar" class="btn btn-danger">Vaciar</button>
-                </aside>
-            </div>
-        </div>
-        `
-        document.body.append(card)
-
-    }
-} */
-
-//cardComun(carrito);
-
-/* function verCarrito() {
-    // Vaciamos todo el html
-    DOMcarrito.textContent = '';
-    // Quitamos los duplicados
-    const carritoSinDuplicados = [...new Set(carrito)];
-    // Generamos los Nodos a partir de carrito
-    carritoSinDuplicados.forEach((item) => {
-        // Obtenemos el item que necesitamos de la variable base de datos
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
-            // ¿Coincide las id? Solo puede existir un caso
-            return itemBaseDatos.id === parseInt(item);
-        });
-        // Cuenta el número de veces que se repite el producto
-        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-            return itemId === item ? total += 1 : total;
-        }, 0);
-        // Creamos el nodo del item del carrito
-        const miNodo = document.createElement('li');
-        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-        // Boton de borrar
-        const miBoton = document.createElement('button');
-        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-        miBoton.textContent = 'X';
-        miBoton.style.marginLeft = '1rem';
-        miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
-        // Mezclamos nodos
-        miNodo.appendChild(miBoton);
-        DOMcarrito.appendChild(miNodo);
-    });
-    // Renderizamos el precio total en el HTML
-    DOMtotal.textContent = calcularTotal();
-}
-
-function borrarItemCarrito(evento) {
-    // Obtenemos el producto ID que hay en el boton pulsado
-    const id = evento.target.dataset.item;
-    // Borramos todos los productos
-    carrito = carrito.filter((carritoId) => {
-        return carritoId !== id;
-    });
-    // volvemos a renderizar
-    renderizarCarrito();
-}
-
-function calcularTotal() {
-    // Recorremos el array del carrito 
-    return carrito.reduce((total, item) => {
-        // De cada elemento obtenemos su precio
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
-            return itemBaseDatos.id === parseInt(item);
-        });
-        // Los sumamos al total
-        return total + miItem[0].precio;
-    }, 0).toFixed(2);
-}
-
-function vaciarCarrito() {
-    // Limpiamos los productos guardados
-    carrito = [];
-    // Renderizamos los cambios
-    renderizarCarrito();
-}
-
-// Eventos
-//DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-
-/* function toastCarrito(prod) {
-Toastify({
-    text: `Se agrego al carrito`,
-    duration: 3000,
-    gravity: `bottom`, // `top` or `bottom`
-    position: `right`, // `left`, center` or `right`
-    onClick: function agregaCarrito(prod) {agregaCarrito(`${e.nombre}`)} // Callback after click
-}).showToast();
-} */
-
-// Inicio
-/* Productos();
-Carrito(); */ 
